d21982aaebfe125b597c526f982006986de2d524
Brian Bock <bbock@terpmail.umd.edu>
2020-04-19 15:14:25 -0400

    added sprite lists to simplify blits

--- game.py
+++ game.py
@@ -15,24 +15,40 @@ bg = pygame.image.load('assets/road.png')
 bgX = 0
 bgX2 = bg.get_width()
 
+all_sprites = pygame.sprite.Group()
+obst_list = pygame.sprite.Group()
+active_list=pygame.sprite.Group()
+
 # Create our car
 orange_car=car(10,10,"protagonist")
+all_sprites.add(orange_car)
 
 # Create active cars
-active_list=[]
 for i in range (0,random.randint(1,3)):
+    tempcar=car(0,0,"dynamic")
     randx=random.randint(100,900)
     randy=random.randint(50,350)
-    active_list.append(car(randx,randy,"dynamic"))
+    # # Eliminate cars on the lane lines
+    # if (100-tempcar.car_height<=randy<=100+tempcar.car_height) or (200-tempcar.car_height<=randy<=200+tempcar.car_height) or (300-tempcar.car_height<=randy<=300+tempcar.car_height):
+    #     randy=random.randint(50,350)
+    active_list.add(car(randx,randy,"dynamic"))
+    all_sprites.add(car(randx,randy,"dynamic"))
+tempcar.kill()
+# obst_list=[]
 
-obst_list=[]
+
 
 def generateRandomObstacle(obst_list):
-        randx=random.randint(1100,2000)
-        randy=random.randint(100,300)
-        obst_list.append(car(randx,randy,"obstacle"))
+    # print("Attempting to spawn a new blue car")
+    randx=random.randint(1000,2000)
+    randy=random.randint(10,350)
+    new_obst=car(randx,randy,"obstacle")
+    # a=pygame.sprite.spritecollide(car(randx,randy,"obstacle"), obst_list, True)
+    # print(a)
+    obst_list.add(new_obst)
+    all_sprites.add(new_obst)
 
-        return obst_list
+    return obst_list
 
 
 
@@ -42,14 +58,12 @@ def redrawGameWindow():
     win.blit(bg, (int(bgX2), 0))  # draws the seconf bg image
 
     # Redraw our car 
-    win.blit(orange_car.car, (orange_car.x,orange_car.y))
+    # win.blit(orange_car.car, (orange_car.x,orange_car.y))
 
     # Redraw other cars
-    for obstacle in obst_list:
-        win.blit(obstacle.car, (int(obstacle.x),int(obstacle.y)))
-
-    for active_car in active_list:
-        win.blit(active_car.car,(int(active_car.x),int(active_car.y)))
+    for sprite in all_sprites:
+        win.blit(sprite.car,(int(sprite.x),int(sprite.y)))
+    # pygame.sprite.spritecollide(orange_car, obst_list, True)
 
     pygame.display.update()
     
@@ -63,15 +77,22 @@ while run:
     bgX -= road_speed  # Move both background images back
     bgX2 -= road_speed
 
-    if len(obst_list)==0:
+    # If there are no blue cars, make some more
+    # print(len(obst_list))
+    if len(obst_list)<3:
         for i in range(0,random.randint(3,10)):
             obst_list=generateRandomObstacle(obst_list)
 
     for obstacle in obst_list:
         obstacle.x-=road_speed
         if obstacle.x < obstacle.car_width * -1: # If our obstacle is off the screen we will remove it
-            obst_list.pop(obst_list.index(obstacle))
-            obst_list=generateRandomObstacle(obst_list)
+            obstacle.kill()
+            obst_list=generateRandomObstacle(obst_list) # create a new obstacle to replace it
+
+    for active_car in active_list:
+        if active_car.x < active_car.car_width * -1: # If our obstacle is off the screen we will remove it
+            active_list.pop(active_list.index(active_car))
+
 
     if bgX < bg.get_width() * -1:  # If our bg is at the -width then reset its position
         bgX = bg.get_width()
@@ -88,17 +109,16 @@ while run:
     if keys[pygame.K_LEFT] and orange_car.x > orange_car.vel: 
         orange_car.x -= orange_car.vel
 
-
     elif keys[pygame.K_RIGHT] and orange_car.x < (canvas_width - orange_car.vel - orange_car.car_width):
         orange_car.x += orange_car.vel
 
-     
     if keys[pygame.K_DOWN] and orange_car.y <(canvas_height-orange_car.vel-orange_car.car_height):
         orange_car.y+=orange_car.vel
 
     elif keys[pygame.K_UP] and orange_car.y > orange_car.vel:
         orange_car.y-=orange_car.vel
 
+    
 
     redrawGameWindow() 
     

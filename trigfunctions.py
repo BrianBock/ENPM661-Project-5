09e9a597ed3043ce57d222b7860a32aec45570dc
Brian Bock <bbock@terpmail.umd.edu>
2020-04-23 11:32:57 -0400

    deg2rad is an np function, not math

--- trigfunctions.py
+++ trigfunctions.py
@@ -1,21 +1,22 @@
 import math
+import numpy as np
 
 def cosd(deg_angle):
-	a=math.cos(math.deg2rad(deg_angle))
+	a=math.cos(np.deg2rad(deg_angle))
 	return a
 
 def sind(deg_angle):
-	a=math.sin(math.deg2rad(deg_angle))
+	a=math.sin(np.deg2rad(deg_angle))
 	return a
 
 def tand(deg_angle):
-	a=math.tan(math.deg2rad(deg_angle))
+	a=math.tan(np.deg2rad(deg_angle))
 	return a
 
 
 def cotd(deg_angle):
-	a=math.sin(math.deg2rad(deg_angle))
-	b=math.cos(math.deg2rad(deg_angle))
+	a=math.sin(np.deg2rad(deg_angle))
+	b=math.cos(np.deg2rad(deg_angle))
 
 	cotd=b/a
 	return cotd

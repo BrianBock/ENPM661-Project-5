bf9d56d337321c5d37c56b658ab74a3e36383fae
Brian Bock <bbock@terpmail.umd.edu>
2020-05-02 12:57:06 -0400

    moved functions to world class

--- game.py
+++ game.py
@@ -1,98 +1,16 @@
 import pygame
-import random
+
 
 from classes import car
-
-class car_game():
-    def __init__(self):
-        pygame.init()
-
-        self.road_speed=1.6
-
-        # Set up canvas
-        self.canvas_width=1000
-        self.canvas_height=400
-        self.win = pygame.display.set_mode((self.canvas_width, self.canvas_height))
-
-        self.clock = pygame.time.Clock()
-        self.run = True
-
-        # Set up background
-        self.bg = pygame.image.load('assets/road.png')
-        self.bgX = 0
-        self.bgX2 = self.bg.get_width()
-
-        self.all_sprites = pygame.sprite.Group()
-        self.obst_list = pygame.sprite.Group()
-        self.active_list=pygame.sprite.Group()
-
-        # Create our car
-        self.orange_car=car(10,10,"protagonist")
-        self.all_sprites.add(self.orange_car)
-
-        # Create stationary cars
-        self.generateBlueCars()
-
-        # Create active cars
-        for i in range (0,random.randint(1,3)):
-            tempcar=car(0,0,"dynamic")
-            randx=random.randint(100,900)
-            randy=random.randint(50,350)
-            # # Eliminate cars on the lane lines
-            # if (100-tempcar.car_height<=randy<=100+tempcar.car_height) or (200-tempcar.car_height<=randy<=200+tempcar.car_height) or (300-tempcar.car_height<=randy<=300+tempcar.car_height):
-            #     randy=random.randint(50,350)
-            self.active_list.add(car(randx,randy,"dynamic"))
-            self.all_sprites.add(car(randx,randy,"dynamic"))
-        tempcar.kill()
-        # obst_list=[]
-
-
-
-    def generateRandomObstacle(self):
-        # print("Attempting to spawn a new blue car")
-        randx=random.randint(1000,2000)
-        randy=random.randint(10,350)
-        new_obst=car(randx,randy,"obstacle")
-        # a=pygame.sprite.spritecollide(car(randx,randy,"obstacle"), obst_list, True)
-        # print(a)
-        self.obst_list.add(new_obst)
-        self.all_sprites.add(new_obst)
-
-        # return self.obst_list
-
-
-
-    def generateBlueCars(self):
-
-        start_pos_list=[(130,220),(200,330),(500,10)]
-
-        for start_pos in start_pos_list:
-            x,y=start_pos
-            new_obst=car(x,y,"obstacle")
-            self.obst_list.add(new_obst)
-            self.all_sprites.add(new_obst)
-
-
-
-
-
-
-    def redrawGameWindow(self):
-        # Redraw background
-        self.win.blit(self.bg, (int(self.bgX), 0))  # draws our first bg image
-        self.win.blit(self.bg, (int(self.bgX2), 0))  # draws the seconf bg image
-
-        # Redraw all cars
-        for sprite in self.all_sprites:
-            self.win.blit(sprite.car_image_new,(int(sprite.spritex),int(sprite.spritey)))
-
-        pygame.display.update()
-    
+from window_class import Window, World, car_game
 
 
 
 
 game=car_game()
+
+# Generate world
+world=World(game)
 
 
 while game.run:
@@ -132,8 +50,8 @@ while game.run:
 
 
     # Move the orange car based on arrow keys
-    keys = pygame.key.get_pressed()
-    game.orange_car.moveCar(keys,(game.canvas_width,game.canvas_height))
+    # keys = pygame.key.get_pressed()
+    # game.orange_car.moveCar(keys,(game.canvas_width,game.canvas_height))
     
 
 
